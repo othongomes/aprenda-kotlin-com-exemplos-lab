c4ab28f405ea724c214d978e4cc73b7928389c6e
othongomes <129392763+othongomes@users.noreply.github.com>
2023-08-07 11:44:55 -0300

    Solução em Koltin abstraindo domínio

--- desafio.kt
+++ desafio.kt
@@ -2,9 +2,9 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
@@ -12,10 +12,29 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     
     fun matricular(usuario: Usuario) {
         TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Aluno ${usuario.nome} matriculado na formação $nome")
     }
 }
 
 fun main() {
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    
+    val aluno1 = Usuario("João")
+    val aluno2 = Usuario("Maria")
+    val aluno3 = Usuario("Pedro")
+
+    val conteudo1 = ConteudoEducacional("Introdução à Programação", 120)
+    val conteudo2 = ConteudoEducacional("Estruturas de Dados", 180)
+    val conteudo3 = ConteudoEducacional("Banco de Dados", 150)
+
+    val formacao = Formacao("Desenvolvimento Web", Nivel.INTERMEDIARIO, listOf(conteudo1, conteudo2, conteudo3))
+
+    formacao.matricular(aluno1)
+    formacao.matricular(aluno2)
+    formacao.matricular(aluno3)
+
+    formacao.listarInscritos()
 }
